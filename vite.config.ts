8bed78dbf12bdd66ebb21a4c04dbeafa2cc164f0
tpkee <89136677+tpkee@users.noreply.github.com>
2025-11-23 23:12:41 +0100

    chore: add packages

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,24 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.startsWith('cropper-'),
+        },
+      },
+    }),
     vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
