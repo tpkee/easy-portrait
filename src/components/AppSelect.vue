79a1fd3dafd6dfe6a26e5c7d77b25eb6a6e57907
tpkee <89136677+tpkee@users.noreply.github.com>
2025-11-23 23:13:25 +0100

    feature: add select component

---
+++ src/components/AppSelect.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <label v-if="label" :for="uid" class="block">
+      {{ label }}
+    </label>
+
+    <select :id="uid" v-bind="$attrs" v-model="modelValue">
+      <slot />
+    </select>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useId } from 'vue'
+
+defineProps<{
+  label?: string
+}>()
+
+const uid = useId()
+
+const modelValue = defineModel()
+</script>
