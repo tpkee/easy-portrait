b3fc5012eeb70a53c000faf6d55615a4ae13419c
tpkee <89136677+tpkee@users.noreply.github.com>
2025-11-23 23:13:15 +0100

    feature: add input component

---
+++ src/components/AppInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <label>
+    <span v-if="label" class="font-medium block mb-px">{{ label }}</span>
+    <input v-model="modelValue" v-bind="$attrs" />
+  </label>
+</template>
+
+<script setup lang="ts">
+// Props
+defineProps<{
+  label?: string
+}>()
+
+const modelValue = defineModel()
+
+//Misc
+defineOptions({
+  inheritAttrs: false,
+})
+</script>
